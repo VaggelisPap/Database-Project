dd0d494f52160c16ccdbde1a6d5a4c732ae15af6
VaggelisPap <61821180+VaggelisPap@users.noreply.github.com>
2022-05-26 19:24:13 +0300

    Add files via upload

---
+++ Field.sql
@@ -0,0 +1,12 @@
+INSERT INTO `Field` (`Field_id`,`name`) VALUES (1,'Maths');
+INSERT INTO `Field` (`Field_id`,`name`) VALUES (2,'Physics');
+INSERT INTO `Field` (`Field_id`,`name`) VALUES (3,'IT');
+INSERT INTO `Field` (`Field_id`,`name`) VALUES (4,'History');
+INSERT INTO `Field` (`Field_id`,`name`) VALUES (5,'Archaeology');
+INSERT INTO `Field` (`Field_id`,`name`) VALUES (6,'Environment');
+INSERT INTO `Field` (`Field_id`,`name`) VALUES (7,'Biology');
+INSERT INTO `Field` (`Field_id`,`name`) VALUES (8,'Social Science');
+INSERT INTO `Field` (`Field_id`,`name`) VALUES (9,'Law');
+INSERT INTO `Field` (`Field_id`,`name`) VALUES (10,'Geology');
+INSERT INTO `Field` (`Field_id`,`name`) VALUES (11,'Politics');
+INSERT INTO `Field` (`Field_id`,`name`) VALUES (12,'Chemistry');
