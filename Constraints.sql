075e19bde87a1d25795ec590c3a42b00f9e26def
VaggelisPap <61821180+VaggelisPap@users.noreply.github.com>
2022-06-05 00:21:38 +0300

    Delete Constraints.sql

--- Constraints.sql
+++
@@ -1,5 +0,0 @@
-create table temp as (select Responsible_id from Projects);
-update Researcher,Projects,temp set Projects.Organization_id = Researcher.Organization_id where Researcher.Researcher_id in (temp.Responsible_id) and Projects.Responsible_id in (temp.Responsible_id);
-update Projects set grade = null,  grade_date = null where end_date > NOW();
-
-
